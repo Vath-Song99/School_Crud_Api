78c3564595bcc0d3f195dbc2ee1a8cbc7f4828f5
Smoeury Songvat <vatgaming287@gmail.com>
2024-03-15 14:58:13 +0700

    status code

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,15 +1,20 @@
 import { Request, Response, NextFunction } from 'express';
+import { BaseCustomError } from './mongoose';
+
 
 // Global error handler middleware
 function errorHandler(err: Error, req: Request, res: Response, next: NextFunction) {
   // Default to 500 if no status code is set
-  const statusCode =  req.statusCode;
-
   //res to client 
-  res.json({
-    statusCode: statusCode,
-    message: err.message,
-  });
+  console.log(err)
+  if (err instanceof BaseCustomError){
+    res.status(err.statusCode).json({
+      statusCode: err.statusCode,
+      
+      message: err.message,
+    });
+  }
+
   next()
 }
 
--- src/middlewares/mongoose.ts
+++ src/middlewares/mongoose.ts
@@ -1,6 +1,18 @@
 import { NextFunction, Request, Response } from "express";
 import mongoose from "mongoose";
 
+export class BaseCustomError extends Error {
+  [x: string]: any;
+  constructor(message: string | undefined, statusCode: number) {
+    super(message); // Call the super constructor (Error class)
+    this.statusCode = statusCode; // Custom property to hold status code
+    this.name = this.constructor.name; // Set the name of the error to the class name
+    Error.captureStackTrace(this, this.constructor); // Capture stack trace
+  }
+}
+
+
+
 const validateMongooseId = (
   req: Request,
   res: Response,
@@ -9,7 +21,9 @@ const validateMongooseId = (
   const { id } = req.params;
 
   if (!mongoose.isValidObjectId(id)) {
-    _next(new Error("Invalid Id"));
+    const customError = new BaseCustomError('id Invalide', 404);
+    console.log(customError.statusCode)
+    _next(customError);
   }
   _next();
 };
